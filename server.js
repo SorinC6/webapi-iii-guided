1d3bf7ac04bd61cfaad321147af0c661bfa5ba18
Sorin Chis <kish.sorin@yahoo.com>
2019-03-20 11:10:17 +0200

    helmet added to server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res, next) => {
-  res.send(`
+	res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome to the Lambda Hubs API</p>
     `);
